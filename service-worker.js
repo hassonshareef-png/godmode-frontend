e94b90243dfb40ee3f6f597630f4a4a0963e44d6
hassyb <hassonshareef@gmail.com>
2026-06-27 15:05:10 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,29 @@
+// Install and cache core files
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("godmode-cache").then((cache) => {
       return cache.addAll([
         "index.html",
         "numbers.html",
-        "manifest.json"
+        "manifest.json",
+        "icon-192.png",
+        "icon-512.png"
       ]);
     })
   );
   self.skipWaiting();
 });
 
+// Activate immediately
 self.addEventListener("activate", (e) => {
   clients.claim();
 });
 
+// Serve from cache first, then network
 self.addEventListener("fetch", (e) => {
   e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
+    caches.match(e.request).then((cached) => {
+      return cached || fetch(e.request);
     })
   );
 });
