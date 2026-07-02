63e79f18ce682f4fc1f0c915830b7c124fe3b814
hassyb <hassonshareef@gmail.com>
2026-07-02 11:50:01 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,6 @@
+<Route path="/" element={<Home />} />
+<Route path="/director-auth" element={<DirectorAuth />} />
+<Route path="/director-dashboard" element={<DirectorDashboard />} />
+import Home from "./pages/Home";
+import DirectorAuth from "./pages/DirectorAuth";
+import DirectorDashboard from "./pages/DirectorDashboard";
